2d48e68b934dc08d6aab32f1ced1f9ea2076890e
VRonaldoC <105024658+VRonaldoC@users.noreply.github.com>
2026-03-27 05:49:24 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
-python3 render_main.py
+
+printf "1\n" | python3 SCRIPT_PERMISO_DESBLOQUEO.py &
+printf "2\n" | python3 SCRIPT_PERMISO_DESBLOQUEO.py &
+printf "3\n" | python3 SCRIPT_PERMISO_DESBLOQUEO.py &
+printf "4\n" | python3 SCRIPT_PERMISO_DESBLOQUEO.py &
+
+wait
